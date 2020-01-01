a648837c2e9916d0544a05612c97131ed0cd5737
Avene <juc-y@live.jp>
2020-01-02 01:20:38 +0900

    Enable some devise options

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :lockable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :timeoutable
+         :recoverable, :rememberable, :validatable, :confirmable, :lockable
 
   has_many :cars, inverse_of: :user
   has_many :maintenances, through: :cars, inverse_of: :user
