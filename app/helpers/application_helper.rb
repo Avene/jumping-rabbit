cf2ef7eca959ef8132081df4524a1c430b6fadaa
Avene <juc-y@live.jp>
2020-01-01 22:45:38 +0900

    Make forms responsive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
         new_object = f.object.send(association).klass.new
         id = new_object.object_id
         fields = f.fields_for(association, new_object, child_index: id) do |builder|
-          render(association.to_s.singularize + "_fields", f: builder)
+          render(association.to_s.singularize + "_fields", form: builder)
         end
         link_to(name, 'JavaScript:void(0);', class: html_class, data: {add_fields: true, target_id: target_id, id: id, fields: fields.gsub("\n", "")})
     end 
