cade90b18bbf9646f65483f49a0a6875f64ee422
Avene <juc-y@live.jp>
2020-01-03 17:57:42 +0900

    Enable case insensitive search

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -3,7 +3,7 @@ class Operation < ApplicationRecord
   has_one :car, through: :maintenance, inverse_of: :operations
   has_one :user, through: :car, inverse_of: :operations
 
-  scope :by_title_like, lambda { |company|
-    where('operations.title LIKE :value', { value: "#{sanitize_sql_like(company)}%"})
+  scope :by_title_like, lambda { |q|
+    where('lower(operations.title) LIKE :q', { q: "#{sanitize_sql_like(q.downcase)}%"})
   }
 end
